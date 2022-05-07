c7c2d20ec425acfad8c4c5be25499e375523a0d3
KorolevEgor <korolevegir@gmail.com>
2022-05-07 10:47:12 +0300

    add suggest method

--- src/main/java/LinksSuggester.java
+++ src/main/java/LinksSuggester.java
@@ -1,49 +1,65 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LinksSuggester {
 
-    private final List<Suggest> suggests = new ArrayList<>();
+    // key word to Suggest
+    private final Map<String, Suggest> suggests = new HashMap<>();
+    private Pattern urlPattern =
+            Pattern.compile("(http://?|https://?|ftp://?|file://?)(www.)?[a-zA-Z][a-zA-Z0-9]+[.][a-zA-Z][a-zA-Z]+[a-zA-Z0-9/+&@#$^%=~_|]+");
 
     public LinksSuggester(File file) throws IOException, WrongLinksFormatException {
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        Pattern urlPattern = Pattern.compile("(http://?|https://?|ftp://?|file://?)(www.)?[a-zA-Z][a-zA-Z0-9]+[.][a-zA-Z][a-zA-Z]+[a-zA-Z0-9/+&@#$^%=~_|]+");
-        String line;
-        while(true){
-            line = br.readLine();
-            if (line == null) break;
-            List<String> words = Arrays.stream(line.split("[\s\t]+")).toList();
-            System.out.println(words);
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String configLine;
+            while (true) {
+                // чтение строки из файла
+                configLine = br.readLine();
+                if (configLine == null) break;
 
-            if (words.size() < 3) {
-                throw new WrongLinksFormatException("invalid string in config: " + words);
+                // разбиение прочитанной строки на слова по пробелу или табу
+                List<String> words = Arrays.stream(configLine.split("[\s\t]+")).toList();
+
+                // должно быть как минимум 3 слова (ключевое слово, заголовок, ссылка)
+                if (words.size() < 3) {
+                    throw new WrongLinksFormatException("invalid string in config: " + words);
+                }
+
+                String url = words.get(words.size() - 1);
+
+                if (!goodUrl(url)) {
+                    throw new WrongLinksFormatException("invalid url in config: " + url);
+                }
+
+                // формирование заголовка из промежуточных слов (первое слово - ключевое слово, последнее - ссылка)
+                StringBuilder titleSB = new StringBuilder();
+                for (int i = 1; i < words.size() - 1; ++i) {
+                    titleSB.append(words.get(i)).append(' ');
+                }
+
+                String keyWord = words.get(0).toLowerCase(Locale.ROOT);
+                suggests.put(keyWord, new Suggest(keyWord, titleSB.toString(), url));
             }
-
-            String url = words.get(words.size() - 1);
-            System.out.println(url);
-
-            Matcher urlMatcher = urlPattern.matcher(url);
-            if (urlMatcher.find()) {
-                System.out.println(url.substring(urlMatcher.start(), urlMatcher.end()));
-            } else {
-                throw new WrongLinksFormatException("invalid url in config: " + url);
-            }
-
-            StringBuilder titleSB = new StringBuilder();
-            for (int i = 1; i < words.size() - 1; ++i) {
-                titleSB.append(words.get(i)).append(' ');
-            }
-            suggests.add(new Suggest(words.get(0).toLowerCase(Locale.ROOT), titleSB.toString(), url));
         }
     }
 
+    // проверка URL на корректность
+    boolean goodUrl(String url) {
+        Matcher urlMatcher = urlPattern.matcher(url);
+//        System.out.println(url.substring(urlMatcher.start(), urlMatcher.end()));
+        return urlMatcher.find();
+    }
+
     public List<Suggest> suggest(String text) {
+        Set<Suggest> resultSet = new HashSet<>();
+        String[] words = text.toLowerCase(Locale.ROOT).split("[\s\t,.:;!?\"'()]+");
+        for (String word : words) {
+            if (suggests.containsKey(word)) {
+                resultSet.add(suggests.get(word));
+            }
+        }
         System.out.println(suggests);
-        return null;
+        return resultSet.stream().toList();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // создаём конфиг
         LinksSuggester linksSuggester = new LinksSuggester(new File("data/config"));
-        linksSuggester.suggest("");
+        System.out.println(linksSuggester.suggest("Тестовый текст. Класс, джава; объект. Spring"));
         // перебираем пдфки в data/pdfs
 
         // для каждой пдфки создаём новую в data/converted
